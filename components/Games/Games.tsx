0fc37a19327a619b392bc5eb26dfc078847105f3
MalcolmDahling <mac.dahling@gmail.com>
2022-12-12 15:37:46 +0100

    Update Games.tsx

--- components/Games/Games.tsx
+++ components/Games/Games.tsx
@@ -20,34 +20,17 @@ const Section = styled('section', {
     display:'grid',
     gridTemplateColumns:'repeat(4, 1fr)',
     gap:20,
+
+    '@indexGamesGrid_3_columns_adRight':{gridTemplateColumns:'repeat(3, 1fr)'},
+    '@indexGamesGrid_5_columns':{gridTemplateColumns:'repeat(5, 1fr)'},
+    '@indexGamesGrid_4_columns':{gridTemplateColumns:'repeat(4, 1fr)'},
+    '@indexGamesGrid_3_columns':{gridTemplateColumns:'repeat(3, 1fr)'},
     '@indexGamesGrid_2_columns':{gridTemplateColumns:'repeat(2, 1fr)'},
 
     variants:{
         marginRight:{
             true:{
                 marginRight:60
-            }
-        },
-
-        grid:{
-            columns_3_adRight:{
-                gridTemplateColumns:'repeat(3, 1fr)',
-            },
-            
-            columns_5:{
-                gridTemplateColumns:'repeat(5, 1fr)',
-            },
-
-            columns_4:{
-                gridTemplateColumns:'repeat(4, 1fr)',
-            },
-
-            columns_3:{
-                gridTemplateColumns:'repeat(auto-fit, calc(33.33% - 20px))',
-            },
-
-            columns_2:{
-                gridTemplateColumns:'repeat(auto-fit, calc(50% - 20px))',
             }
         }
     }
@@ -91,16 +74,7 @@ export default function Games(props:props){
 
 
     return(
-        <Section
-            marginRight={props.marginRight}
-            grid={{
-                '@indexGamesGrid_3_columns_adRight' : 'columns_3_adRight',
-                '@indexGamesGrid_5_columns' : 'columns_5',
-                '@indexGamesGrid_4_columns' : 'columns_4',
-                '@indexGamesGrid_3_columns' : 'columns_3',
-                '@indexGamesGrid_2_columns' : 'columns_2',
-            }}
-        >
+        <Section marginRight={props.marginRight}>
 
             {indexCurrentTab === 'Now Available' && fetchedGames.map((game, i) => {
 
